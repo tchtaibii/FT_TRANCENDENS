4cf8db1f15c1bcf1451e062134d64ad222a4c169
Taha Chtaibi <tchtaibi@e1r3p2.1337.ma>
2023-07-15 01:40:43 +0100

    block

--- front-end/src/components/Home/components/Main/Settings/BlockedList.tsx
+++ front-end/src/components/Home/components/Main/Settings/BlockedList.tsx
@@ -1,20 +1,37 @@
+import { useEffect, useState } from 'react';
 import './Settings.scss'
+import axios from '../../../../../Interceptor/Interceptor'
 
 
-function UserB() {
-    return (
-        <div className="userBlocked">
-            <div className="infoUser">
-                <img src="" alt="" />
-                <p>npetter</p>
-            </div>
-            <button>
-                <div className="unblockBTN">Unblock</div>
-            </button>
-        </div>
-    )
-}
+
 function Blockedlist() {
+    const [update, setUpdate] = useState(true);
+    function UserB(props: any) {
+        const Deblock = async () => {
+            console.log(props.friendshipId )
+            await axios.post('/CancelRequest', { FriendshipId: props.idShip })
+            setUpdate(!update)
+        }
+        return (
+            <div className="userBlocked">
+                <div className="infoUser">
+                    <img src={props.avatar} alt="" />
+                    <p>{props.username}</p>
+                </div>
+                <button onClick={Deblock}>
+                    <div className="unblockBTN">Unblock</div>
+                </button>
+            </div>
+        )
+    }
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const FetchData = async () => {
+            await axios.get("/setting/blockedlist").then(resp => setData(resp.data))
+        };
+        FetchData();
+    }, [update])
     return (
         <div className='BlockedList'>
 
@@ -22,19 +39,9 @@ function Blockedlist() {
                 <h1>Blocked Account</h1>
                 <div className="blockedListC">
                     <div className="usersBlocked">
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
-                        <UserB />
+                        {
+                            data.length > 0 ? data.map((e: any, number) => <UserB key={number + 'userBlocked'} idShip={e.friendshipId} avatar={e.avatar} username={e.username} />) : <p>No User Blocked</p>
+                        }
                     </div>
                 </div>
 

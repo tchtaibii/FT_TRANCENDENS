ad765501df0326dd28a1282174996a4897f10ab6
Taha Chtaibi <tchtaibi@e2r6p7.1337.ma>
2023-08-05 14:47:03 +0100

    again

--- back-end/prisma/migrations/20230802032906_asda/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterEnum
-ALTER TYPE "notificationType" ADD VALUE 'Message';
-
--- AlterTable
-ALTER TABLE "Membership" ADD COLUMN     "unmuteUntil" TIMESTAMP(3),
-ALTER COLUMN "Role" SET DEFAULT 'Member';
